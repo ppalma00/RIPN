76af72886e35647515b0cc124e3dd00616406650
ppalma00 <pedro.sanchez@upct.es>
2025-11-11 09:27:37 +0100

    Delete src/PN directory

--- src/PN/Observer.java
+++
@@ -1,7 +0,0 @@
-package PN;
-
-public interface Observer {
-    void onDiscreteActionExecuted(String actionName, double[] parameters);
-    void onDurativeActionStarted(String actionName, double[] parameters);
-    void onDurativeActionStopped(String actionName);
-}
